510276cb4ddf33258014461336f9f23f29a80cbd
Marco Vermeulen <vermeulen.mp@gmail.com>
2021-11-18 08:43:00 +0000

    Move home help message to help library.

---
+++ src/help_messages.rs
@@ -0,0 +1,200 @@
+pub const CONFIG_HELP: &str = "\
+sdk config
+
+An sdk subcommand to edit the SDKMAN configuration file.
+
+USAGE:
+    sdk config
+
+This subcommand opens a text editor on the configuration file located at
+${SDKMAN_DIR}/etc/config. The subcommand will infer the text editor from the
+EDITOR environment variable. If the system does not set the EDITOR environment
+variable, then vi is assumed as the default editor.
+
+EXAMPLE:
+    $ sdk config
+";
+
+pub const DEFAULT_HELP: &str = "\
+sdk default
+
+An sdk subcommand to set the local default version of the candidate.
+
+USAGE:
+    sdk default <CANDIDATE> [VERSION]
+
+The mandatory candidate qualifier of the subcommand specifies the candidate to
+default for all future shells.
+
+The optional version qualifier set that specific version as default for all
+subsequent shells on the local environment. Omitting the version will set the
+global SDKMAN tracked version as the default version for that candidate.
+
+The subcommand will return a non-zero return code if the candidate or version
+does not exist.
+
+The shorthand mnemonic 'd' is provided in the place of the default subcommand.
+
+EXAMPLE:
+    $ sdk default java 17.0.0-tem
+    $ sdk default java
+";
+
+pub const HOME_HELP: &str = "\
+sdk home
+
+An sdk subcommand, used in scripting to output the location of a specific
+candidate version.
+
+USAGE:
+    sdk home <CANDIDATE> <VERSION>
+
+Print the absolute home path of any candidate version installed by SDKMAN. The
+candidate and version parameters are mandatory. The subcommand will emit a
+non-zero exit code if a valid candidate version is not locally installed.
+
+EXAMPLE:
+    $ sdk home java 17.0.0-tem
+";
+
+pub const INSTALL_HELP: &str = "\
+sdk install
+
+The sdk subcommand to install a candidate version.
+
+USAGE:
+    sdk install <CANDIDATE> [VERSION] [LOCAL_PATH]
+
+Invoking this subcommand with only the candidate as a parameter will install the
+currently known default version for that candidate.
+
+Provide a subsequent qualifier to install a specific non-default version. This
+subcommand will return a non-zero exit code for unfound versions.
+
+Provide another qualifier to add an already installed local version. This
+qualifier is the absolute local path to the base directory of the SDK to be
+added. The local version will appear as an installed version of the candidate.
+The version may not conflict with an existing version, installed or not. The
+subcommand will return a non-zero exit code if the directory does not exist.
+
+The shorthand mnemonic 'i' is provided in the place of the install subcommand.
+
+EXAMPLES:
+    $ sdk install java
+    $ sdk install java 17.0.0-tem
+    $ sdk install java 11-local /usr/lib/jvm/java-11-openjdk
+";
+
+pub const LIST_HELP: &str = "\
+sdk list
+
+The sdk subcommand to list all candidates or candidate versions.
+
+USAGE:
+    sdk list [CANDIDATE]
+
+Invoke the subcommand without a candidate to see a comprehensive list of all
+candidates with name, URL, detailed description and an installation command.
+
+If the candidate qualifier is specified, the subcommand will display a list of
+all available and local versions for that candidate. In addition, the version
+list view marks all versions that are local, installed or currently in use.
+They appear as follows:
+
++ - local version
+* - installed
+> - currently in use
+
+Java has a custom list view with vendor-specific details.
+
+The alias 'ls' is provided as a shorthand alternative to list.
+
+EXAMPLE:
+    $ sdk list
+    $ sdk list java
+    $ sdk list groovy
+";
+
+pub const MAIN_HELP: &str = "\
+sdk - The command line interface (CLI) for SDKMAN!
+
+USAGE:
+    sdk <SUBCOMMAND> [CANDIDATE] [VERSION]
+
+SDKMAN! is a tool for managing parallel versions of multiple Software
+Development Kits on most Unix based systems. It provides a convenient Command
+Line Interface (CLI) and API for installing, switching, removing and listing
+Candidates.
+
+SUBCOMMANDS:
+    help              [SUBCOMMAND]
+    install   or i    <CANDIDATE> [VERSION] [LOCAL_PATH]
+    uninstall or rm   <CANDIDATE> <VERSION>
+    list      or ls   [CANDIDATE]
+    use       or u    <CANDIDATE> <VERSION>
+    config
+    default   or d    <CANDIDATE> [VERSION]
+    home      or h    <CANDIDATE> <VERSION>
+    env       or e    [INIT|INSTALL|CLEAR]
+    current   or c    [CANDIDATE]
+    upgrade   or ug   [CANDIDATE]
+    version   or v
+    broadcast or b
+    offline           [ENABLE|DISABLE]
+    selfupdate        [FORCE]
+    update
+    flush             [ARCHIVE|TMP|BROADCAST|METADATA|VERSION]
+
+EXAMPLES:
+    $ sdk install java 17.0.0-tem
+    $ sdk help install
+";
+
+pub const UNINSTALL_HELP: &str = "\
+sdk uninstall
+
+The sdk subcommand to uninstall a candidate version.
+
+USAGE:
+    sdk uninstall <CANDIDATE> <VERSION>
+
+Always follow the subcommand with two qualifiers, the candidate and version to
+be uninstalled.
+
+The specified version will be removed from the candidate directory in
+$SDKMAN_DIR/candidates and will no longer be available for use on the system.
+The binary archive for the SDK will be cached in the archives folder if
+re-installation is required later.
+
+An invalid candidate or version supplied to the subcommand will result in a
+non-zero return code.
+
+The alias 'rm' is provided as a shorthand alternative to uninstall.
+
+EXAMPLE:
+    $ sdk uninstall java 17.0.0-tem
+";
+
+pub const USE_HELP: &str = "\
+sdk use
+
+An sdk subcommand to use a specific version in the current shell.
+
+USAGE:
+    sdk use <CANDIDATE> <VERSION>
+
+The mandatory candidate and version follow the subcommand to specify what to
+use in the current shell.
+
+This subcommand only operates on the current shell. It does not affect other
+shells running different versions of the same candidate. It also does not change
+the default version set for all subsequent new shells.
+
+The subcommand will return a non-zero return code if the candidate or version
+does not exist.
+
+The shorthand mnemonic 'u' is provided in the place of the use subcommand.
+
+EXAMPLE:
+    $ sdk use java 17.0.0-tem
+";
