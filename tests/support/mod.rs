25458eec53306880b5dfc52e663dd1d26c93cbca
Marco Vermeulen <vermeulen.mp@gmail.com>
2022-06-20 15:35:04 +0100

    Handle edge cases for version and bolster tests.

--- tests/support/mod.rs
+++ tests/support/mod.rs
@@ -1,13 +1,26 @@
-use std::fmt::Error;
 use std::fs::{create_dir, File};
 use std::io::Write;
+use std::path::Path;
+
 use tempfile::{Builder, TempDir};
 
-pub fn virtual_env(version: String) -> Result<TempDir, Error> {
-    let sdkman_dir = Builder::new().prefix(".sdkman-").tempdir().unwrap();
-    create_dir(sdkman_dir.path().join("var")).unwrap();
-    let version_file_path = sdkman_dir.path().join("var").join("version");
+pub fn virtual_env(version: String) -> TempDir {
+    let sdkman_dir = prepare_sdkman_dir();
+    init_var_dir(sdkman_dir.path());
+    write_version_file(sdkman_dir.path(), version);
+    return sdkman_dir;
+}
+
+pub fn prepare_sdkman_dir() -> TempDir {
+    Builder::new().prefix(".sdkman-").tempdir().unwrap()
+}
+
+pub fn init_var_dir(temp_dir: &Path) {
+    create_dir(temp_dir.join("var")).unwrap();
+}
+
+pub fn write_version_file(temp_dir: &Path, version: String) {
+    let version_file_path = temp_dir.join("var").join("version");
     let mut version_file = File::create(version_file_path).expect("could not create file");
     write!(version_file, "{}", version.to_string()).unwrap();
-    return Ok(sdkman_dir);
 }
--- tests/version.rs
+++ tests/version.rs
@@ -4,17 +4,19 @@ use std::process::Command;
 
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
+use serial_test::serial;
 
 mod support;
 
 #[test]
+#[serial]
 fn should_successfully_render_version() -> Result<(), Box<dyn std::error::Error>> {
     let prefix = "SDKMAN";
     let version = "5.0.0";
 
     let header = format!("\n{}", prefix);
 
-    let sdkman_dir = support::virtual_env(version.to_string()).unwrap();
+    let sdkman_dir = support::virtual_env(version.to_string());
 
     env::set_var("SDKMAN_DIR", sdkman_dir.path().as_os_str());
 
@@ -23,5 +25,36 @@ fn should_successfully_render_version() -> Result<(), Box<dyn std::error::Error>
         .success()
         .stdout(predicate::str::starts_with(header).and(predicate::str::contains(version)))
         .code(0);
+
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn should_panic_if_version_file_not_present() -> Result<(), Box<dyn std::error::Error>> {
+    let sdkman_dir = support::prepare_sdkman_dir();
+
+    env::set_var("SDKMAN_DIR", sdkman_dir.path().as_os_str());
+
+    Command::cargo_bin("version")?
+        .assert()
+        .failure()
+        .code(78);
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn should_panic_if_version_file_empty() -> Result<(), Box<dyn std::error::Error>> {
+    let sdkman_dir = support::prepare_sdkman_dir();
+    support::init_var_dir(sdkman_dir.path());
+    support::write_version_file(sdkman_dir.path(), "".to_string());
+
+    env::set_var("SDKMAN_DIR", sdkman_dir.path().as_os_str());
+
+    Command::cargo_bin("version")?
+        .assert()
+        .failure()
+        .code(78);
     Ok(())
 }
