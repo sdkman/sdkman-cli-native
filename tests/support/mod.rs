0a1630603f1920f735cbb175615df4bee8323cff
Marco Vermeulen <vermeulen.mp@gmail.com>
2022-06-21 17:50:45 +0100

    Add support for native extensions in version subcommand.

--- tests/support/mod.rs
+++ tests/support/mod.rs
@@ -4,10 +4,16 @@ use std::path::Path;
 
 use tempfile::{Builder, TempDir};
 
-pub fn virtual_env(version: String) -> TempDir {
+pub fn virtual_env(version: String, native_version: String) -> TempDir {
     let sdkman_dir = prepare_sdkman_dir();
     init_var_dir(sdkman_dir.path());
-    write_version_file(sdkman_dir.path(), version);
+
+    let version_file = Path::new("var/version");
+    write_file(sdkman_dir.path(), version_file, version.to_owned());
+
+    let native_version_file = Path::new("var/version_native");
+    write_file(sdkman_dir.path(), native_version_file, native_version.to_owned());
+
     return sdkman_dir;
 }
 
@@ -19,8 +25,8 @@ pub fn init_var_dir(temp_dir: &Path) {
     create_dir(temp_dir.join("var")).unwrap();
 }
 
-pub fn write_version_file(temp_dir: &Path, version: String) {
-    let version_file_path = temp_dir.join("var").join("version");
-    let mut version_file = File::create(version_file_path).expect("could not create file");
-    write!(version_file, "{}", version.to_string()).unwrap();
+pub fn write_file(temp_dir: &Path, relative_path: &Path, content: String) {
+    let absolute_path = temp_dir.join(relative_path);
+    let mut version_file = File::create(absolute_path).expect("could not create file");
+    write!(version_file, "{}", content.to_string()).unwrap();
 }
