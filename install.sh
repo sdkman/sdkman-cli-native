d6eb77ee002bae1aae10516702f1f084ea9c40e5
XS <tonycody@qq.com>
2022-09-28 17:36:02 +0800

    :pencil2: Installation script, installation directory error fix.

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+# To build first
 cargo build
 
+# Checking environment variables
 if [[ ! -d "$SDKMAN_DIR" ]]; then
   export SDKMAN_DIR="$HOME/.sdkman"
   echo "SDKMAN_DIR environment variable not defined, set to $SDKMAN_DIR"
 fi
 
-find target/debug -maxdepth 1 -executable -type f -exec cp -v {} "$SDKMAN_DIR/libexec" \;
+# Check the directory
+if [[ ! -d "$SDKMAN_DIR/libexec" ]]; then
+  mkdir -p "$SDKMAN_DIR/libexec"
+  echo "Libexec directory does not exist, created to $SDKMAN_DIR/libexec"
+fi
+
+find target/debug -maxdepth 1 ! -name ".cargo-lock" ! -name "*.d" -executable -type f -exec cp -v {} "$SDKMAN_DIR/libexec/" \;
