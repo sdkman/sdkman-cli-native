85123ea44a4f86cce331916a15f4290c88d1dd1f
Marco Vermeulen <vermeulen.mp@gmail.com>
2023-02-21 22:05:20 +0000

    Add dummy broadcast command.

---
+++ src/bin/broadcast/main.rs
@@ -0,0 +1,14 @@
+use colored::Colorize;
+
+fn main() {
+    println!("");
+    println!("We've removed the {} command!", "broadcast".bold());
+    println!("");
+    println!(
+        "Please follow {} on Twitter for all release announcements.",
+        "@sdkman_".bold()
+    );
+    println!("");
+    println!("{}", "https://www.twitter.com/sdkman_".italic());
+    println!("")
+}
