df1f85c16b36476bcebd0e4b1262939847f9113e
Marco Vermeulen <vermeulen.mp@gmail.com>
2023-05-09 20:14:17 +0100

    Reinstate path joins.

--- src/bin/uninstall/main.rs
+++ src/bin/uninstall/main.rs
@@ -1,12 +1,11 @@
 use std::fs;
-use std::path::PathBuf;
 use std::process;
 
 use clap::Parser;
 use colored::Colorize;
-use sdkman_cli_native::constants::{CANDIDATES_DIR, CURRENT_DIR};
 use symlink::remove_symlink_dir;
 
+use sdkman_cli_native::constants::{CANDIDATES_DIR, CURRENT_DIR};
 use sdkman_cli_native::helpers::{infer_sdkman_dir, known_candidates};
 
 #[derive(Parser, Debug)]
@@ -30,7 +29,7 @@ fn main() {
     let candidate = args.candidate;
     let version = args.version;
     let force = args.force;
-    let sdkman_dir: PathBuf = infer_sdkman_dir();
+    let sdkman_dir = infer_sdkman_dir();
 
     let all_candidates = known_candidates(sdkman_dir.to_owned());
     if !all_candidates.contains(&candidate.as_str()) {
@@ -38,13 +37,9 @@ fn main() {
         process::exit(1);
     }
 
-    let inferred_dir = infer_sdkman_dir();
-    let sdkman_dir = inferred_dir.to_str().unwrap();
-
-    let candidate_dir = format!("{}/{}/{}", sdkman_dir, CANDIDATES_DIR, candidate);
-    let version_dir = format!("{}/{}", candidate_dir, version);
-    let version_path = PathBuf::from(version_dir);
-    let current_link_path = PathBuf::from(format!("{}/{}", candidate_dir, CURRENT_DIR));
+    let candidate_path = sdkman_dir.join(CANDIDATES_DIR).join(&candidate);
+    let version_path = sdkman_dir.join(&candidate_path).join(&version);
+    let current_link_path = candidate_path.join(CURRENT_DIR);
     if current_link_path.is_dir() {
         let canonical_link_path =
             fs::read_link(current_link_path.to_owned()).expect("panic! can't read link");
--- src/bin/version/main.rs
+++ src/bin/version/main.rs
@@ -1,25 +1,24 @@
 extern crate core;
 
 use colored::Colorize;
+
 use sdkman_cli_native::helpers::check_exists;
 use sdkman_cli_native::{
     constants::VAR_DIR,
     helpers::{infer_sdkman_dir, read_file_content},
 };
-use std::path::PathBuf;
 
 const CLI_VERSION_FILE: &str = "version";
 const NATIVE_VERSION_FILE: &str = "version_native";
 
 fn main() {
-    let inferred_dir = infer_sdkman_dir();
-    let sdkman_dir = inferred_dir.to_str().unwrap();
+    let sdkman_dir = infer_sdkman_dir();
 
-    let cli_version = read_version_file(format!("{}/{}/{}", sdkman_dir, VAR_DIR, CLI_VERSION_FILE));
-    let native_version = read_version_file(format!(
-        "{}/{}/{}",
-        sdkman_dir, VAR_DIR, NATIVE_VERSION_FILE
-    ));
+    let cli_version_file = sdkman_dir.join(VAR_DIR).join(CLI_VERSION_FILE);
+    let cli_version = read_file_content(check_exists(cli_version_file));
+
+    let native_version_file = sdkman_dir.join(VAR_DIR).join(NATIVE_VERSION_FILE);
+    let native_version = read_file_content(check_exists(native_version_file));
 
     match (cli_version, native_version) {
         (Some(cli), Some(native)) => println!(
@@ -30,10 +29,4 @@ fn main() {
         ),
         _ => std::process::exit(exitcode::CONFIG),
     }
-}
-
-fn read_version_file(file_location: String) -> Option<String> {
-    let version_path = PathBuf::from(file_location);
-    let verified_path = check_exists(version_path);
-    read_file_content(verified_path)
 }
