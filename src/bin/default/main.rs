e82195ce725b2ad3f1580fa5cb489b54a812f452
Marco Vermeulen <vermeulen.mp@gmail.com>
2023-07-11 23:09:22 +0100

    Introduce default command.

---
+++ src/bin/default/main.rs
@@ -0,0 +1,56 @@
+use fs::symlink;
+use std::fs::remove_dir_all;
+use std::os::unix::fs;
+
+use clap::Parser;
+use colored::Colorize;
+use symlink::remove_symlink_dir;
+
+use sdkman_cli_native::constants::{CANDIDATES_DIR, CURRENT_DIR};
+use sdkman_cli_native::helpers::{
+    infer_sdkman_dir, known_candidates, validate_candidate, validate_version_path,
+};
+
+#[derive(Parser, Debug)]
+#[command(
+    bin_name = "sdk default",
+    about = "sdk subcommand to set the local default version of the candidate"
+)]
+struct Args {
+    #[arg(required(true))]
+    candidate: String,
+
+    #[arg(required(true))]
+    version: String,
+}
+fn main() {
+    let args = Args::parse();
+    let candidate = args.candidate;
+    let version = args.version;
+    let sdkman_dir = infer_sdkman_dir();
+    let candidate = validate_candidate(known_candidates(sdkman_dir.to_owned()), &candidate);
+    let version_path = validate_version_path(sdkman_dir.to_owned(), &candidate, &version);
+    let current_link_path = sdkman_dir
+        .join(CANDIDATES_DIR)
+        .join(&candidate)
+        .join(CURRENT_DIR);
+
+    if current_link_path.exists() {
+        remove_symlink_dir(current_link_path.to_owned()).unwrap_or_else(|_| {
+            remove_dir_all(current_link_path.to_owned()).expect(&format!(
+                "cannot remove current directory for {}",
+                candidate
+            ))
+        })
+    }
+    symlink(version_path, current_link_path)
+        .map(|_| {
+            println!(
+                "set {} {} as {} version",
+                &candidate.bold(),
+                &version.bold(),
+                "default".italic()
+            )
+        })
+        .expect("cannot create symlink");
+}
---
+++ tests/default.rs
@@ -0,0 +1,119 @@
+#[cfg(test)]
+use assert_cmd::Command;
+use predicates::str::contains;
+use serial_test::serial;
+use std::{env, fs};
+use support::{TestCandidate, VirtualEnv};
+
+mod support;
+
+#[test]
+#[serial]
+fn should_set_an_installed_version_as_default() -> Result<(), Box<dyn std::error::Error>> {
+    let candidate = TestCandidate {
+        name: "scala",
+        versions: vec!["0.0.1", "0.0.2"],
+        current_version: "0.0.1",
+    };
+    let env = VirtualEnv {
+        cli_version: "0.0.1".to_string(),
+        native_version: "0.0.1".to_string(),
+        candidate: Some(candidate),
+    };
+
+    let sdkman_dir = support::virtual_env(env);
+    let dir_string = sdkman_dir.path().to_str().unwrap();
+
+    env::set_var("SDKMAN_DIR", dir_string);
+    let expected_output = "set scala 0.0.2 as default";
+    Command::cargo_bin("default")?
+        .arg("scala")
+        .arg("0.0.2")
+        .assert()
+        .success()
+        .stdout(contains(expected_output))
+        .code(0);
+
+    let file = sdkman_dir
+        .path()
+        .join("candidates")
+        .join("scala")
+        .join("current")
+        .join("bin")
+        .join("scala");
+    let content = fs::read_to_string(file).unwrap();
+    assert!(content.contains("Running scala 0.0.2"));
+
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn should_reset_the_current_default_version_as_default() -> Result<(), Box<dyn std::error::Error>> {
+    let candidate = TestCandidate {
+        name: "scala",
+        versions: vec!["0.0.1"],
+        current_version: "0.0.1",
+    };
+    let env = VirtualEnv {
+        cli_version: "0.0.1".to_string(),
+        native_version: "0.0.1".to_string(),
+        candidate: Some(candidate),
+    };
+
+    let sdkman_dir = support::virtual_env(env);
+    let dir_string = sdkman_dir.path().to_str().unwrap();
+
+    env::set_var("SDKMAN_DIR", dir_string);
+    let expected_output = "set scala 0.0.1 as default version";
+    Command::cargo_bin("default")?
+        .arg("scala")
+        .arg("0.0.1")
+        .assert()
+        .success()
+        .stdout(contains(expected_output))
+        .code(0);
+
+    let file = sdkman_dir
+        .path()
+        .join("candidates")
+        .join("scala")
+        .join("current")
+        .join("bin")
+        .join("scala");
+    let content = fs::read_to_string(file).unwrap();
+    assert!(content.contains("Running scala 0.0.1"));
+
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn should_not_set_an_uninstalled_version_as_default() -> Result<(), Box<dyn std::error::Error>> {
+    {
+        let candidate = TestCandidate {
+            name: "scala",
+            versions: vec!["0.0.1"],
+            current_version: "0.0.1",
+        };
+        let env = VirtualEnv {
+            cli_version: "0.0.1".to_string(),
+            native_version: "0.0.1".to_string(),
+            candidate: Some(candidate),
+        };
+
+        let sdkman_dir = support::virtual_env(env);
+        let dir_string = sdkman_dir.path().to_str().unwrap();
+
+        env::set_var("SDKMAN_DIR", dir_string);
+        let expected_output = "scala 0.0.2 is not installed on your system";
+        Command::cargo_bin("default")?
+            .arg("scala")
+            .arg("0.0.2")
+            .assert()
+            .failure()
+            .stderr(contains(expected_output))
+            .code(1);
+        Ok(())
+    }
+}
