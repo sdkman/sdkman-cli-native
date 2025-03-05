b83cb8533e4b228e118f780cea2996850e03ba0d
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-03-05 21:59:35 +0000

    Introduce channel-specific release defaulting to beta

--- bin/release-binary.sh
+++ bin/release-binary.sh
@@ -4,16 +4,21 @@ MONGO_URL="$1"
 MONGO_USERNAME="$2"
 MONGO_PASSWORD="$3"
 RELEASE_VERSION="$4"
+RELEASE_CHANNEL="$5"
 
-if [[ -z "$MONGO_URL" || -z "$MONGO_USERNAME" || -z "$MONGO_PASSWORD" || -z "$RELEASE_VERSION" ]]; then
-  echo "Cannot release stableNativeCliVersion: $RELEASE_VERSION"
-	echo "Missing parameters..."
+if [[ -z "$MONGO_URL" || -z "$MONGO_USERNAME" || -z "$MONGO_PASSWORD" || -z "$RELEASE_VERSION" || -z "$RELEASE_CHANNEL" ]]; then
+  echo "Cannot perform release!"
+  echo "Missing parameters..."
 else
-  echo "Releasing stableNativeCliVersion: $RELEASE_VERSION"
-  echo "Releasing betaNativeCliVersion: $RELEASE_VERSION"
+  echo "Releasing $RELEASE_CHANNEL: $RELEASE_VERSION"
+  case "$RELEASE_CHANNEL" in
+    beta)   NATIVE_FIELD="betaNativeCliVersion" ;;
+    stable) NATIVE_FIELD="stableNativeCliVersion" ;;
+  esac
+  echo "Updating field: $NATIVE_FIELD"
   docker run mongo:3.2 mongo "${MONGO_URL}" \
     --username="${MONGO_USERNAME}" \
     --password="${MONGO_PASSWORD}" \
     --quiet \
-    --eval "db.application.updateOne({}, {\$set: { \"stableNativeCliVersion\": \"$RELEASE_VERSION\", \"betaNativeCliVersion\": \"$RELEASE_VERSION\"}});"
+    --eval "db.application.updateOne({}, {\$set: { \"$NATIVE_FIELD\": \"$RELEASE_VERSION\"}});"
 fi
